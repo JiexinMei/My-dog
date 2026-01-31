1699cec9dad8fda8d3b92513cb6f428f5e96ddb1
JiexinMei <jiexin3803@gmail.com>
2026-01-31 18:10:53 +0000

    dog

---
+++ my-dog.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(225, 120, 130);
+fill('red')
+ rect(250, 210, 15, 70);
+ rect(230, 210, 15, 70);
+ rect(130, 210, 15, 70);
+ rect(150, 210, 15, 70);
+fill('purple')
+rect(260, 130, 10, 70);
+fill("blue")
+ellipse(200, 200, 160, 80);
+fill("green")
+circle(120, 160, 80);
+fill("white")
+circle(100, 160, 10);
+circle(140, 160, 10);
+triangle (135, 200, 140, 180, 80, 170)
+}
